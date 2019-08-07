f7f32ed4b55f85dbdfa9d7b5d9a37344a9550693
ThisLiss <52800098+ThisLiss@users.noreply.github.com>
2019-08-07 17:48:08 +0400

    fixes

--- app/src/main/java/com/example/myapplications/Adapter.java
+++ app/src/main/java/com/example/myapplications/Adapter.java
@@ -1,8 +1,6 @@
 package com.example.myapplications;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
@@ -11,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -19,16 +16,30 @@ import java.util.List;
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     private Context contextAd;
-    private PackageManager pm;
-    private List packages;
-    private static int size;
-    private LinearLayout view;
+    private test con;
+    private PackageManager pmA;
+    private List packagesA;
+    private static int sizeA;
 
-    public Adapter(Context context){
+    public Adapter(Context context, PackageManager pm, List packages, int size){
         contextAd = context;
-        pm = context.getPackageManager();
-        packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-        size = packages.size();
+        pmA = pm;
+        packagesA = packages;
+        sizeA = size;
+    }
+
+
+    public interface test{
+        void item(int pos);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        Context context = recyclerView.getContext();
+        if(context instanceof test)
+            con = (test)context;
+
     }
 
     @NonNull
@@ -52,7 +63,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return size;
+        return sizeA;
     }
 
     class ViewHolder extends RecyclerView.ViewHolder{
@@ -61,71 +72,28 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         TextView packageIndex;
         ImageView iconView;
 
-        ImageView iconDialog;
-        TextView textPack;
-        TextView textName;
-        TextView textVer;
-        TextView textVerCode;
-
-        String versionName = null;
-        int versionCode = 0;
-
         private ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            nameView = itemView.findViewById(R.id.app_name);
-            packageIndex= itemView.findViewById(R.id.app_package);
-            iconView = itemView.findViewById(R.id.app_icon);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    LayoutInflater inflater = LayoutInflater.from(contextAd);
-                    view = (LinearLayout) inflater.inflate(R.layout.for_dialog, null);
-
-                    iconDialog = view.findViewById(R.id.imageView);
-                    textName = view.findViewById(R.id.textName);
-                    textPack = view.findViewById(R.id.textPackage);
-                    textVer = view.findViewById(R.id.textVer);
-                    textVerCode = view.findViewById(R.id.textVerCode);
-
-                    int position = getAdapterPosition();
-                    ApplicationInfo app = (ApplicationInfo) packages.get(position);
-
-                    try {
-                        versionName = pm.getPackageInfo(app.packageName, 0).versionName;
-                        versionCode = pm.getPackageInfo(app.packageName, 0).versionCode;
-                    } catch (PackageManager.NameNotFoundException e) {
-                        e.printStackTrace();
-                    }
-
-                    AlertDialog.Builder Dialog = new AlertDialog.Builder(contextAd);
-                    Dialog.setView(view);
-
-                    iconDialog.setImageDrawable(app.loadIcon(pm));
-                    textName.setText(app.loadLabel(pm).toString());
-                    textPack.setText(app.packageName);
-                    textVer.setText(versionName);
-                    textVerCode.setText(versionCode+"");
-
-                    Dialog.setNegativeButton(
-                            "Закрыть", new DialogInterface.OnClickListener(){
-                                public void onClick(DialogInterface dialog, int which){
-                                    dialog.cancel();
-                                }
-                            });
-
-                    Dialog.show();
+                    con.item(getAdapterPosition());
                 }
             });
+
+            nameView = itemView.findViewById(R.id.app_name);
+            packageIndex= itemView.findViewById(R.id.app_package);
+            iconView = itemView.findViewById(R.id.app_icon);
         }
 
 
         void bind(int listIndex){
-            ApplicationInfo app = (ApplicationInfo) packages.get(listIndex);
-            nameView.setText(app.loadLabel(pm).toString());
+            ApplicationInfo app = (ApplicationInfo) packagesA.get(listIndex);
+            nameView.setText(app.loadLabel(pmA).toString());
             packageIndex.setText(app.packageName);
-            iconView.setImageDrawable(app.loadIcon(pm));
+            iconView.setImageDrawable(app.loadIcon(pmA));
         }
     }
 }
--- app/src/main/java/com/example/myapplications/MainActivity.java
+++ app/src/main/java/com/example/myapplications/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.myapplications;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Adapter.test {
 
     private RecyclerView numbersList;
     private Adapter adapter;
+    private PackageManager pm;
+    private List packages;
+    private static int size;
+    private LinearLayout view;
+
+    ImageView iconDialog;
+    TextView textPack;
+    TextView textName;
+    TextView textVer;
+    TextView textVerCode;
+    String versionName = null;
+    int versionCode = 0;
+    ApplicationInfo app;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +45,54 @@ public class MainActivity extends AppCompatActivity {
         numbersList = findViewById(R.id.rv_numbers);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         numbersList.setLayoutManager(layoutManager);
-        adapter = new Adapter(MainActivity.this);
+
+        pm = MainActivity.this.getPackageManager();
+        packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        size = packages.size();
+
+        adapter = new Adapter(MainActivity.this, pm, packages, size);
         numbersList.setAdapter(adapter);
+
     }
 
+    @Override
+    public void item(int position) {
+
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                view = (LinearLayout) inflater.inflate(R.layout.for_dialog, null);
+
+                iconDialog = view.findViewById(R.id.imageView);
+                textName = view.findViewById(R.id.textName);
+                textPack = view.findViewById(R.id.textPackage);
+                textVer = view.findViewById(R.id.textVer);
+                textVerCode = view.findViewById(R.id.textVerCode);
+
+
+                app = (ApplicationInfo) packages.get(position);
+
+                try {
+                    versionName = pm.getPackageInfo(app.packageName, 0).versionName;
+                    versionCode = pm.getPackageInfo(app.packageName, 0).versionCode;
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                AlertDialog.Builder Dialog = new AlertDialog.Builder(MainActivity.this);
+                Dialog.setView(view);
+
+                iconDialog.setImageDrawable(app.loadIcon(pm));
+                textName.setText(app.loadLabel(pm).toString());
+                textPack.setText(app.packageName);
+                textVer.setText(versionName);
+                textVerCode.setText(versionCode + "");
+
+                Dialog.setNegativeButton(
+                        "Закрыть", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                Dialog.show();
+        }
 }
